0cebcd346b511c5216b1217506d5b8c4aa945fc6
Easton Potokar <contagon6@gmail.com>
2021-12-04 04:15:05 +0000

    Got jacob, ik, ik_callback all functioning :)

--- README.md
+++ README.md
@@ -31,3 +31,14 @@ $ source devel/setup.bash
 $ cd src/
 $ git clone git@github.com:contagon/eezy_arm_ros.git
 ```
+
+# Usage
+After running `catkin_make` and `source devel/setup.bash`, to launch all nodes, run
+```shell
+$ roslaunch eezy_arm_ros start.launch
+```
+Then, to send a desired position or joint angle, run one of the following commands
+```shell
+$ rostopic pub -1 /arm1/p_des geometry_msgs/Vector3 -- 200 -100 250
+$ rostopic pub -1 /arm1/q_des eezy_arm_ros/Joints -- 0 0 0
+```
--- nodes/kinematics.py
+++ nodes/kinematics.py
@@ -5,6 +5,7 @@ import numpy as np
 # ROS imports
 import rospy
 from geometry_msgs.msg import Vector3, Pose, Point, Quaternion
+from eezy_arm_ros.msg import Joints, JointsArray, Vector3Array
 
 NUM_JOINTS = 3
 
@@ -88,11 +89,8 @@ def translate_se3(t_row_vector : np.array) -> np.array:
     Returns:
         np.array: 4x4 transformation matrix (translation only)
     """   
-    R = np.eye(3)
-    # "The -1 on reshape is a broadcast-like, using all possible elements, and the 1 creates the 
-    # second required dimension." -- https://stackoverflow.com/a/53304024
-    T = np.concatenate((R, t_row_vector.reshape(-1,1)), axis=1)
-    T = np.concatenate((T, np.array([0, 0, 0, 1])), axis=0)
+    T = np.eye(4)
+    T[:3,3] = t_row_vector
     return T
 
 
@@ -109,11 +107,19 @@ class EezyBotArm:
         self._l3 = 147  # mm
         self._l4 = 87  # mm
 
-        self.base_to_first_joint = translate_se3(np.array([0, 0, 50.]))
+        self.base_to_first_joint = translate_se3(np.array([0, 0, 0]))
+
+        self.curr_q = np.zeros(3)
 
         # Set up ROS publishers and subscribers
+        self.q_subscriber = rospy.Subscriber("/q", Joints, self.fk_callback)
         self.fk_pub = rospy.Publisher("/end_effector_pose", Pose, queue_size=10)
-        self.q_subscriber = rospy.Subscriber("/q_des", Vector3, self.fk_callback)
+
+        self.ik_sub = rospy.Subscriber("/p_des", Vector3, self.ik_callback)
+        self.ik_traj_sub = rospy.Subscriber("/p_des_traj", Vector3Array, self.ik_traj_callback)
+
+        self.q_des_pub = rospy.Publisher("/q_des", Joints, queue_size=10)
+        self.q_des_traj_pub = rospy.Publisher("/q_des_traj", JointsArray, queue_size=10)
 
     def compute_A_matrix(self, q : list, joint_idx : int) -> np.array:
         """Computes the transformation matrix from the given joint to the end of the link. With the 
@@ -132,7 +138,7 @@ class EezyBotArm:
         if joint_idx == 0:
             A = rotate_z_se3(q[0]) @ translate_se3(np.array([0, 0, self._l1])) @ rotate_x_se3(-np.pi/2, radians=True)
         elif joint_idx == 1:
-            A = rotate_z_se3(q[1]) @ rotate_z_se3(-np.pi/2, radians=True) @ translate_se3(np.array([self._l2, 0, 0])) @ rotate_z_se3(-np.pi/2, radians=True) @ rotate_z_se3(-q[1])
+            A = rotate_z_se3(q[1]) @ rotate_z_se3(-np.pi/2, radians=True) @ translate_se3(np.array([self._l2, 0, 0])) @ rotate_z_se3(np.pi/2, radians=True) @ rotate_z_se3(-q[1])
         elif joint_idx == 2:
             A = rotate_z_se3(q[2]) @ translate_se3(np.array([self._l3, 0, 0])) @ rotate_z_se3(-q[2])
         elif joint_idx == 3:
@@ -141,14 +147,103 @@ class EezyBotArm:
             raise ValueError('Invalid joint! Expects 0 - 3')
 
         return A
-    
-    def fk_callback(self, msg : Vector3):
-        """ROS callback for computing forward kinematics. Subscribes to the desired joint configuration.
+
+    def forward_kinematics(self, q : list, starting_link=0, ending_link=NUM_JOINTS, base=True, ee=True) -> np.array:
+        """Returns the transformation matrix from one link to another given a q.
+           Defaults to calculating fk from the base to the end effector.
 
         Args:
-            msg (Vector3): ROS message containing the current joint angles
+            q (list): Joint angles in degrees.
+            starting_link (int, optional): The link from which to calculate fk. Defaults to 0 (base frame).
+            ending_frame (int, optional): The link to which we want fk calcualted. Defaults to NUM_JOINTS (end effector frame).
+            base (bool, optional): If true and starting index is 0, then base transform will be included. Defaults to True.
+            ee (bool, optional): If true and index ends at nth frame, then end effector transforma will be included. Defaults to True.
+
+        Returns:
+            np.array: 4x4 transformation matrix from the 'starting_link' to the 'ending_frame'
         """
-        q = [msg.x, msg.y, msg.z]
+        if base == True and starting_link == 0:
+            T = self.base_to_first_joint
+        else:
+            T = np.eye(4) 
+
+        for i in range(starting_link, ending_link):
+            T = T @ self.compute_A_matrix(q, i)
+
+        if ee == True and ending_link == NUM_JOINTS:
+            T = T @ self.compute_A_matrix(q, 3)
+
+        return T
+
+    def jacob(self, q : list, idx=NUM_JOINTS, base=True, ee=True) -> np.array:
+        """Returns the Jacobian of link at idx in the base frame at configuration q.
+
+        Args:
+            q (list) : Joint angles in degrees.
+            idx (int, optional): The link to which we want the Jacobian. Defaults to NUM_JOINTS (ee frame).
+            base (bool, optional): If true and starting index is 0, then base transform will be included. Defaults to True.
+            ee (bool, optional): If true and index ends at nth frame, then end effector transforma will be included. Defaults to True.
+
+        Returns:
+            np.array: 6x3 Jacobian of idx in the base frame.
+        """
+        J = np.zeros((6,3))
+
+        Pe = self.forward_kinematics(q, ending_link=idx, base=base, ee=ee)[:3,3]
+
+        for i in range(idx):
+            f = self.forward_kinematics(q, ending_link=i, base=base, ee=ee)
+            zi = f[:3,2]
+
+            J[0:3, i] = np.cross(zi, Pe-f[:3,3])
+            J[3:6, i] = zi
+
+        return J
+
+    def ik(self, p_des : list, q0 : list, max_iter=1000, tol=1e-6, K=5, method="damped") -> np.array:
+        """Returns the required configuration q to be at position q_des.
+
+        Args:
+            p_des (list) : Desired position (x,y,z) in mm.
+            q0 (list) : Initial configuration starting estimate.
+            max_iter (int, optional) : Maximum number of iterations to perform. Defaults to 1000.
+            tol (float, optional) : Stopping criteria. Algorithm stops if |error|^2 < tol. Defaults to 1e-6.
+            K (float, optional): Gains for algorithm. Defaults to 5.
+            method (str, optional) : IK method to use, either "damped" or "transpose". Defaults to "damped".
+
+        """
+        # setup initial conditions
+        p_des = np.array(p_des, dtype='float')
+        q = np.array(q0, dtype='float').copy()
+        kd2 = np.eye(3)*.001
+        if isinstance(K, float) or isinstance(K, int):
+            K = np.eye(3)*K
+
+        e = p_des - self.forward_kinematics(q)[:3,3]
+
+        for i in range(max_iter):
+            J = self.jacob(q)[:3,:]
+
+            if method == "damped":
+                # qdot = J.T @ np.linalg.inv(J@J.T + kd2) @ K @ e
+                # Since J is square for us, we can just invert:)
+                qdot = np.linalg.inv(J + kd2) @ K @ e
+            elif method == "transpose":
+                qdot = J.T @ K @ e
+
+            q += qdot
+
+            e = p_des - self.forward_kinematics(q)[:3,3]
+            if e@e < tol:
+                return q		
+
+    def fk_callback(self, data : Joints):
+        """ROS callback for computing forward kinematics. Subscribes to the current joint configuration.
+
+        Args:
+            data (Joints): ROS message containing the current joint angles
+        """
+        q = [msg.q1, msg.q2, msg.q3]
         T_ee = self.forward_kinematics(q)
         
         t = Point()
@@ -171,32 +266,20 @@ class EezyBotArm:
 
         self.fk_pub.publish(msg_out)
 
-    def forward_kinematics(self, q : list, starting_link=0, ending_link=NUM_JOINTS, base=True, ee=True) -> np.array:
-        """Returns the transformation matrix from one link to another given the desired joint configuration (self.q_des).
-           Defaults to calculating fk from the base to the end effector.
+    def ik_callback(self, data : Vector3):
+        """ROS callback for computing inverse kinematics. Subscribes to the current ee position.
 
         Args:
-            q (list): Joint angles
-            starting_link (int, optional): The link from which to calculate fk. Defaults to 0 (base frame).
-            ending_frame (int, optional): The link to which we want fk calcualted. Defaults to NUM_JOINTS (end effector frame).
-            base (bool, optional): If true and starting index is 0, then base transform will be included. Defaults to True.
-            ee (bool, optional): If true and index ends at nth frame, then end effector transforma will be included. Defaults to True.
-
-        Returns:
-            np.array: 4x4 transformation matrix from the 'starting_joint' to the 'ending_frame'
+            data (Vector3): ROS message containing the x,y,z coordinate
         """
-        if base == True and starting_link == 0:
-            T = self.base_to_first_joint
-        else:
-            T = np.eye(4) 
+        p_des = [data.x, data.y, data.z]
+        q = self.ik(p_des, self.curr_q)
 
-        for i in range(starting_link, ending_link):
-            T = T @ self.compute_A_matrix(q, i)
+        msg = Joints(q[0], q[1], q[2])
+        self.q_des_pub.publish(msg)
 
-        if ee == True and ending_link == NUM_JOINTS:
-            T = T @ self.compute_A_matrix(q, 3)
-
-        return T
+    def ik_traj_callback(self, data):
+        pass
 
 if __name__ == "__main__":
     rospy.init_node("kinematics")
